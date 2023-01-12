9becb149e56a4d29160d44d7cbb2c6034a80b8ca
Tuan-LeHoang <106129968+Tuan-LeHoang@users.noreply.github.com>
2023-01-12 15:31:54 +0900

    add merge sort
    Need to clarify the merge function in the future to make it more understandable!

--- Sorting/code.cpp
+++ Sorting/code.cpp
@@ -27,12 +27,81 @@ void insertion_sort(int arr[], int n)
     }
 }
 
+void merge(int arr[], int const left, int const mid, int const right)
+{
+    auto const sub_array_one = mid - left + 1;
+    auto const sub_array_two = right - mid;
+
+    auto *left_array = new int[sub_array_one];
+    auto *right_array = new int[sub_array_two];
+
+    //Copy data to temp arrays
+    for(auto i = 0; i < sub_array_one; i++)
+    {
+        left_array[i] = arr[left+i];
+    }
+    for(auto j = 0; j < sub_array_two; j++)
+    {
+        right_array[j] = arr[mid + 1 + j];
+    }
+
+    auto index_of_sub_array_one  = 0, index_of_sub_array_two = 0;
+    int index_of_merged_array = left;
+
+    while(index_of_sub_array_one < sub_array_one && index_of_sub_array_two < sub_array_two)
+    {
+        if(left_array[index_of_sub_array_one] <= right_array[index_of_sub_array_two])
+        {
+            arr[index_of_merged_array] = left_array[index_of_sub_array_one];
+            index_of_sub_array_one++;
+        }
+        else
+        {
+            arr[index_of_merged_array] = right_array[index_of_sub_array_two];
+            index_of_sub_array_two++;
+        }
+        index_of_merged_array++;
+    }
+
+    while(index_of_sub_array_one < sub_array_one)
+    {
+        arr[index_of_merged_array] =  left_array[index_of_sub_array_one];
+        index_of_sub_array_one++;
+        index_of_merged_array++;
+    }
+
+    while(index_of_sub_array_two < sub_array_two)
+    {
+        arr[index_of_merged_array] = right_array[index_of_sub_array_two];
+        index_of_sub_array_two++;
+        index_of_merged_array++;
+    }
+
+    delete[] left_array;
+    delete[] right_array;
+}
+
+
+void merge_sort(int arr[], int const begin, int const end)
+{
+    if(begin >= end)
+    {
+        return;
+    }
+
+    auto mid = (begin + end) / 2;
+    merge_sort(arr,begin, mid);
+    merge_sort(arr, mid + 1, end);
+    merge(arr, begin, mid, end);
+}
+
 int main()
 {
     int arr[] = {4,6,7,2,67,9,0};
     int n = sizeof(arr)/sizeof(arr[0]);
     
-    insertion_sort(arr, n);
+    // insertion_sort(arr, n);
+    merge_sort(arr,0,n-1);
     
     print_arr(arr,n);
     
